922d4494c20c439d806cb005b6b82adf6aee9f7a
Dmitriy <118775938+Dmitryyyyys@users.noreply.github.com>
2026-05-16 15:35:43 +0300

    Update jwt.config.ts

--- src/config/jwt.config.ts
+++ src/config/jwt.config.ts
@@ -1,7 +1,7 @@
 import { JwtModuleOptions } from '@nestjs/jwt';
 
 export const jwtConfig = (): JwtModuleOptions => ({
-  secret: process.env.JWT_SECRET || 'beatstore_secret',
+  secret: process.env.JWT_SECRET,
   signOptions: {
     expiresIn: process.env.JWT_EXPIRES_IN || '24h',
   },
